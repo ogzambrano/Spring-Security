25c1dbadaab4810b806e2c0c83d41e71de3198da
oscarzambrano <ogzambranop@gmial.com>
2023-01-17 10:22:35 -0400

    inicial

---
+++ src/main/java/com/example/springsecurity/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.springsecurity;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hola")
+    public String holamundo(){ return "hola mundo how are you?"; }
+
+    @GetMapping("/bosostrap")
+    public String boostrap(){ return "soy una pagina"; }
+}
+
+
