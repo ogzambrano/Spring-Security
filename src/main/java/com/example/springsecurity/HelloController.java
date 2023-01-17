cebaa5abafe6ff249bd17e4d5d424be7018d00fb
oscarzambrano <ogzambranop@gmial.com>
2023-01-17 15:18:28 -0400

    condiciones de seguridad

--- src/main/java/com/example/springsecurity/HelloController.java
+++ src/main/java/com/example/springsecurity/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     @GetMapping("/hola")
     public String holamundo(){ return "hola mundo how are you?"; }
 
-    @GetMapping("/bosostrap")
+    @GetMapping("/boostrap")
     public String boostrap(){ return "soy una pagina"; }
 }
 
--- src/main/java/com/example/springsecurity/webSecurtyConfig.java
+++ src/main/java/com/example/springsecurity/webSecurtyConfig.java
@@ -1,11 +1,16 @@
 package com.example.springsecurity;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @Configuration
 @EnableWebSecurity
@@ -14,7 +19,8 @@ public class webSecurtyConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/hola").permitAll()//permitir url sin autenticar
+                .antMatchers("/hola").permitAll()//permitir peticion sin autenticar
+                //.antMatchers("/boostrap").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -22,4 +28,27 @@ public class webSecurtyConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic()
         ;
     }
+    @Bean
+    public HttpFirewall looseHttpFirewall(){
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowBackSlash(true);
+        firewall.setAllowSemicolon(true);
+        firewall.setAllowUrlEncodedSlash(true);
+
+        return firewall;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()//crea usuarios en memoria para el acceso a las paginas
+                .withUser("admin").password("password").roles("USER","ADMIN");
+    }
+/*
+    @Bean
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }*/
 }
